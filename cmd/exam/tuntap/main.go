3cce7d7ef18ec7948d6f163a017ae72e67ff02c8
github kkk777-7 <kota.kimura0725@gmail.com>
2023-10-30 07:41:00 +0000

    Add: check tuntap cmd

---
+++ cmd/exam/tuntap/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/kkk777-7/gopher-tcpip/pkg/raw/tuntap"
+)
+
+var devName string
+
+func init() {
+	flag.StringVar(&devName, "name", "", "device name")
+}
+
+func main() {
+	flag.Parse()
+	if devName == "" {
+		fmt.Println("please device name: ./cmd/exam/tuntap --name xxx")
+		os.Exit(1)
+	}
+
+	tap, err := tuntap.NewTap(devName)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("name=%s, addr=%x\n", tap.Name(), tap.Address())
+	buf := make([]byte, 4096)
+	for {
+		n, err := tap.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("--- [%s] incomming %d bytes data ---\n", tap.Name(), n)
+		fmt.Printf("%s", hex.Dump(buf[:n]))
+	}
+}
